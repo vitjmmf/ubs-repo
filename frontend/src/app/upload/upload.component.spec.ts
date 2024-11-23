65a88f494460d2908c802977187c166c3c3d4c90
Marco Mazzei <marco.mazzei@fatec.sp.gov.br>
2024-11-23 08:58:11 -0300

    Ajustes da configuração para inclusão do código de upload

--- frontend/src/app/upload/upload.component.spec.ts
+++ frontend/src/app/upload/upload.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {MatIconModule} from '@angular/material/icon';
+
 import { UploadComponent } from './upload.component';
 
 describe('UploadComponent', () => {
